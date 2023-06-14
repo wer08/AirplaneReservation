232fc4d0fb6c87a6032eeb4599e24031dda3afad
Wojtek <wz08061992@gmail.com>
2023-06-14 13:39:55 +0200

    Added JWt Service

---
+++ backend/src/main/java/com/wojtekzolkowski/airplanereservationsystem/config/JwtService.java
@@ -0,0 +1,34 @@
+package com.wojtekzolkowski.airplanereservationsystem.config;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+
+@Service
+public class JwtService
+{
+    private static final String SECRET_KEY = "uAq3cEFjW8X3z8Ym8PWce0ShsN4GwSLn";
+    public String extractUsername(String token)
+    {
+        return null;
+    }
+
+    private Claims extractAllClaims(String token){
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private Key getSignInKey()
+    {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}
