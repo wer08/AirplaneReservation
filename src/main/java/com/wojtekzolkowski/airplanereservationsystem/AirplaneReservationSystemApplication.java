cd8c778606dd3b5a4842f37750b6ec70aa616908
Wojtek <wz08061992@gmail.com>
2023-06-14 08:25:09 +0200

    Initial commit

---
+++ src/main/java/com/wojtekzolkowski/airplanereservationsystem/AirplaneReservationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.wojtekzolkowski.airplanereservationsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirplaneReservationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirplaneReservationSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wojtekzolkowski/airplanereservationsystem/AirplaneReservationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wojtekzolkowski.airplanereservationsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirplaneReservationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
